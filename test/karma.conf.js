16a864e8f53d9d91b56f9975681ca0e7d395733d
Dong Nguyen <ndaidong@gmail.com>
2016-04-19 22:15:25 +0700

    Add Map polyfill

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,18 +1,20 @@
 module.exports = (config) => {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
-    reporters: ['progress'],
+    frameworks: [ 'jasmine' ],
+    reporters: [ 'progress' ],
     port: 9876,
     colors: true,
     logLevel: config.LOG_ERROR,
     autoWatch: false,
     singleRun: true,
     browsers: [
+      'Chrome',
+      'Firefox',
       'PhantomJS'
     ],
     files: [
-      '../dist/vdom.min.js' , 'specs/*.js'
+      '../dist/vdom.min.js', 'specs/*.js'
     ],
     exclude: [],
     preprocessors: {}
