20208830d50817c8c80ced1a540329bc796901a0
Dong Nguyen <ndaidong@gmail.com>
2017-05-30 00:23:30 +0700

    v2.0.2
    - Rebuild
    - Update travis config

--- dist/realdom.js
+++ dist/realdom.js
@@ -1,7 +1,7 @@
 /**
  * realdom
- * v2.0.1
- * built: Mon, 29 May 2017 16:28:10 GMT
+ * v2.0.2
+ * built: Mon, 29 May 2017 17:22:49 GMT
  * git: https://github.com/ndaidong/realdom
  * author: @ndaidong
  * License: MIT
